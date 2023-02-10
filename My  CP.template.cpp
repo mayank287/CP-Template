91c5c5f2762ebe9fa460b3470cfb3a4349d75018
Mayank Arora <83607303+mayank287@users.noreply.github.com>
2023-02-11 00:17:46 +0530

    Update My  CP.template.cpp

--- My  CP.template.cpp
+++ My  CP.template.cpp
@@ -114,6 +114,15 @@ template <class T>void print(vector<vector<T>> &a){ cerr << "[ ";for (auto i : a
 // ********************************** Some Commmonly Used Functions ********************************* //
 ll GCD(ll a, ll b){while (b) { a %= b;swap(a, b);} return a;}
 ll LCM(ll a,ll b) {return ((ll) a * b) / GCD(a,b);}
+bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
+ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
+ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;
+bool sortd(const pair<int,int> &a,const pair<int,int> &b){return (a.second > b.second);}
+string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
+ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
+bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
+bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
 
 // ************************************* Online Judge **************************** //
 void judge()
@@ -135,8 +144,6 @@ void judge()
 
 void Mayank() 
 {
- 
-
 
 
 }
